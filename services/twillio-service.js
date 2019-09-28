ea73029fbec7757a7256d611800a4d6473a913d8
David Stein <davidmfstein@gmail.com>
2019-09-28 19:26:20 -0400

    send message passed from request

--- services/twillio-service.js
+++ services/twillio-service.js
@@ -3,14 +3,13 @@ const client = new twilio(process.env.TWILLIO_ID, process.env.TWILLIO_TOKEN);
 
 module.exports = app => {
 
-    sendMessage = () => {
-        console.log(process.env.TWILLIO_ID, process.env.TWILLIO_TOKEN);
+    sendMessage = (req, res) => {
         return client.messages.create({
-            body: 'Early Morning Notification',
+            body: req.body.name,
             to: '+16083209137',  // Text this number
             from: '+14242310779' // From a valid Twilio number
         })
-        .then((message) => console.log(message.sid));
+        .then((message) => res.send(message));
     }
     
   

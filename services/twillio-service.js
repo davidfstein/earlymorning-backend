4775c1ff91eb95b4255a3858a9f04edf25ef2f7a
David Stein <davidmfstein@gmail.com>
2019-09-28 19:38:35 -0400

    fix

--- services/twillio-service.js
+++ services/twillio-service.js
@@ -4,7 +4,7 @@ const client = new twilio(process.env.TWILLIO_ID, process.env.TWILLIO_TOKEN);
 module.exports = app => {
 
     sendMessage = (req, res) => {
-        console.log(req.body.patient)
+        console.log(req.body)
         return client.messages.create({
             body: req.body.patient,
             to: '+16083209137',  // Text this number

5c90ff3da823290b62651c4a4fe8965d04d25157
David Stein <davidmfstein@gmail.com>
2019-09-28 17:50:59 -0400

    work

--- services/twillio-service.js
+++ services/twillio-service.js
@@ -1,15 +1,17 @@
 const twilio = require('twilio');
-var client = new twilio(process.env.TWILLIO_ID, process.env.TWILLIO_TOKEN);
+const client = new twilio(process.env.TWILLIO_ID, process.env.TWILLIO_TOKEN);
 
 module.exports = app => {
 
-    sendMessage = (_, res) =>
-        client.messages.create({
+    sendMessage = (_, res) => {
+        console.log('hello');
+        return client.messages.create({
             body: 'Early Morning Notification',
             to: '+16083209137',  // Text this number
             from: '+14242310779' // From a valid Twilio number
         })
         .then((message) => console.log(message.sid));
+    }
     
   
     app.post('/api/message', sendMessage);
